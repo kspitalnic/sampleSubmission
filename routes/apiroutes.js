d7a3f048321e27cb8053d5f20662fdd3b8d4da25
Kate Spitalnic <kespitalnic@gmail.com>
2022-02-09 20:29:43 -0500

    filter by queery participant code id or name

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,14 +1,25 @@
-const router = require('express').Router(); 
+const router = require('express').Router();
+const { participants } = require('../db/participants.json');
+const { findById, filterByQuery } = require('../lib/index.js');
 
 
-router.get('/samples/:id', (req, res) => {
-    const result = findById(req.params.id, samples);
-    if (result) {
-      res.json(result);
-    } else {
-      res.send(404);
+router.get('/participants', (req, res) => {
+    let results = participants;
+    if (req.query) {
+      results = filterByQuery(req.query, results);
     }
+    res.json(results);
   });
+
+
+// router.get('/participants/:id', (req, res) => {
+//     const result = findById(req.params.id, participants);
+//     if (result) {
+//       res.json(result);
+//     } else {
+//       res.send(404);
+//     }
+//   });
 
 
 module.exports = router; 
