cbac1aaf8f72d4cf3b8396d330abea3611899b0c
Kate Spitalnic <kespitalnic@gmail.com>
2022-02-15 07:37:03 -0500

    removed pseudo data

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,14 +1,18 @@
 const router = require('express').Router();
-const { findById, filterByQuery } = require('../lib/index.js');
+const { findByCode, findByID } = require('../lib/index.js');
 const fs = require("fs");
 let csvToJson = require('convert-csv-to-json');
 
 
-let json = csvToJson.parseSubArray('*', ',').getJsonFromCsv('users.csv');
-console.log(json);
-for (let i = 0; i < json.length; i++) {
-  console.log(json[i]);
-}
+
+
+
+
+// let json = csvToJson.parseSubArray('*', ',').getJsonFromCsv('users.csv');
+// console.log(json);
+// for (let i = 0; i < json.length; i++) {
+//   console.log(json[i]);
+// }
 
 
 
@@ -29,8 +33,22 @@ router.get('/participants', (req, res) => {
 router.get('/participants/:code', (req, res) => {
   let json = csvToJson.parseSubArray('*', ',').getJsonFromCsv('users.csv');
 
-  const result = findById(req.params.code, json);
-  console.log(req.params.code);
+  const result = findByCode(req.params.code, json);
+  console.log("code" + req.params.code);
+  console.log(json);
+  console.log(result);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+router.get('/participants/:ID', (req, res) => {
+  let json = csvToJson.parseSubArray('*', ',').getJsonFromCsv('users.csv');
+
+  const result = findByID(req.params.id, json);
+  console.log("ID" + req.params.id);
   console.log(json);
   console.log(result);
   if (result) {
